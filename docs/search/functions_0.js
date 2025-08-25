f91cfe1012c29ce1a1159cd06a4fcd4972e746df
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-08-25 07:57:25 +0000

    Deploying to gh-pages from @ libscran/topicks@8da3c31091d99a31e2b1e40bbd5e8e7433791221 🚀

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['pick_5ftop_5fgenes_0',['pick_top_genes',['../topicks_8hpp.html#a11c8762c3f623432392ad556fa17c745',1,'topicks::pick_top_genes(std::size_t n, const Stat_ *statistic, Top_ top, bool larger, Bool_ *output, const PickTopGenesOptions&lt; Stat_ &gt; &amp;options)'],['../topicks_8hpp.html#ae57428d284301914d408e8b037d5129c',1,'topicks::pick_top_genes(std::size_t n, const Stat_ *statistic, Top_ top, bool larger, const PickTopGenesOptions&lt; Stat_ &gt; &amp;options)']]],
-  ['pick_5ftop_5fgenes_5findex_1',['pick_top_genes_index',['../topicks_8hpp.html#a112b715c087db9dfc69b46f18b8a9227',1,'topicks']]]
+  ['pick_5ftop_5fgenes_0',['pick_top_genes',['../topicks_8hpp.html#a4ea4cd990dc41c02bd9468444223d0c2',1,'topicks::pick_top_genes(const std::size_t n, const Stat_ *const statistic, const Top_ top, const bool larger, Bool_ *const output, const PickTopGenesOptions&lt; Stat_ &gt; &amp;options)'],['../topicks_8hpp.html#a7c5d00047210395e71958231f316c4e2',1,'topicks::pick_top_genes(const std::size_t n, const Stat_ *const statistic, const Top_ top, const bool larger, const PickTopGenesOptions&lt; Stat_ &gt; &amp;options)']]],
+  ['pick_5ftop_5fgenes_5findex_1',['pick_top_genes_index',['../topicks_8hpp.html#ab06b18dc4daabfee067eb5ba301a9558',1,'topicks']]]
 ];
